4556ed8f1948e97bacc4e65766bceb5b02880ecd
Arani Bhaskararao <barani@breakthroughss.com>
2020-08-10 16:02:13 +0530

    Converting into parent child relation
    Converting into parent child relation

---
+++ src/main/java/springboot_jpa/springboot_jpa/App.java
@@ -0,0 +1,13 @@
+package springboot_jpa.springboot_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
